8d2c0126faa0db8be9c6c5ded4145df2dfc17c45
Kenji Doering <kdoering42@gmail.com>
2017-03-14 15:38:33 -0400

    Adding final changes to homework

--- BEE6940Hmwk3.R
+++ BEE6940Hmwk3.R
@@ -102,22 +102,79 @@ g1 <- as.numeric(as.character(OTprcp$Gage1))
 g2 <- as.numeric(as.character(OTprcp$Gage2))
 g3 <- as.numeric(as.character(OTprcp$Gage3))
 g4 <- as.numeric(as.character(OTprcp$Gage4))
+ln_gage1 <-scale(log(as.numeric(as.character(OTprcp$Gage1))),center=TRUE,scale=TRUE)
+ln_gage2 <-scale(log(as.numeric(as.character(OTprcp$Gage2))),center=TRUE,scale=TRUE)
+ln_gage3 <-scale(log(as.numeric(as.character(OTprcp$Gage3))),center=TRUE,scale=TRUE)
+ln_gage4 <-scale(log(as.numeric(as.character(OTprcp$Gage4))),center=TRUE,scale=TRUE)
 
 datmat <- cbind(g1,g2,g3,g4)
-pick <- 4
-mu1 <- mean(datmat[,pick],na.rm = T)
-dat2<- datmat[,-pick]
-datmeans <- colMeans(dat2,na.rm=T)
-dat<-cbind(datmat[,pick],dat2)
-covy <- cov(dat,use='pairwise.complete')
-munew <- mu1 + covy[2:4,1]%*%solve(covy[2:4,2:4])%*%(dat[9,2:4]-datmeans)
+#lndatmat <- cbind(ln_gage1,ln_gage2,ln_gage3,ln_gage4)
+#pick <- 4
+#mu1 <- mean(datmat[,pick],na.rm = T)
+#dat2<- datmat[,-pick]
+#datmeans <- colMeans(dat2,na.rm=T)
+#dat<-cbind(datmat[,pick],dat2)
+#covy <- cov(dat,use='pairwise.complete')
+#munew <- mu1 + covy[2:4,1]%*%solve(covy[2:4,2:4])%*%(dat[9,2:4]-datmeans)
 
-g1NA<-which(is.na(g1)==TRUE)
-g2NA<-which(is.na(g2)==TRUE)
-g3NA<-which(is.na(g3)==TRUE)
-g4NA<-which(is.na(g4)==TRUE)
+#g1NA<-which(is.na(g1)==TRUE)
+#g2NA<-which(is.na(g2)==TRUE)
+#g3NA<-which(is.na(g3)==TRUE)
+#g4NA<-which(is.na(g4)==TRUE)
 
-gap_filler(x,cov,)
+gagedat<-gap_filling(datmat)
+
+setdiff(gagedat[,1],g1)
+ setdiff(gagedat[,2],g2)
+ setdiff(gagedat[,3],g3)
+ setdiff(gagedat[,4],g4)
+
+# MULTIVARIATE INFERENCE --------------------------------------------------
+
+par(mfrow = c(2,2))
+time <- c(1:31)
+plot(time,gagedat[,1],ylim = c(25,60))
+plot(time,gagedat[,2],ylim = c(25,60))
+plot(time,gagedat[,3],ylim = c(25,60))
+plot(time,gagedat[,4],ylim = c(25,60))
+
+mod1<-lm(gagedat[,1]~time)
+mod2<-lm(gagedat[,2]~time)
+mod3<-lm(gagedat[,3]~time)
+mod4<-lm(gagedat[,4]~time)
+
+
+plot(time,gagedat[,1],ylim = c(25,60),main="Gage1 Precip")
+abline(coef(mod1))
+plot(time,gagedat[,2],ylim = c(25,60),main="Gage2 Precip")
+abline(coef(mod2))
+plot(time,gagedat[,3],ylim = c(25,60),main="Gage3 Precip")
+abline(coef(mod3))
+plot(time,gagedat[,4],ylim = c(25,60),main="Gage4 Precip")
+abline(coef(mod4))
+
+
+
+# Partitioning the Data ---------------------------------------------------
+
+
+first <- gagedat[1:15,]
+last <- gagedat[16:31,]
+
+fmeans <- colMeans(first)
+lmeans <- colMeans(last)
+
+fcov <- cov(first)
+lcov <- cov(last)
+
+covpooled <-14/29*fcov + 15/29*lcov
+covdelta <- (1/15 + 1/16)*covpooled
+
+Tdist<-(fmeans-lmeans)%*%solve(covdelta)%*%(fmeans-lmeans)
+
+Fstat <- qf(.95,df1=4,df2=26)
+Fscaled <- Fstat*(33/26)
+
 
 
 
--- gap_filling.R
+++ gap_filling.R
@@ -3,10 +3,42 @@
 gap_filling <- function(x){ 
   
   for (i in 1:nrow(datmat)){
-    
-    
+    if (is.na(sum(x[i,]))){
+      nacols <- which(is.na(x[i,])==TRUE)
+      sub1 <-x[,nacols]
+      sub2 <-x[,-nacols]
+      newx <- cbind(sub1,sub2)
+      covy <- cov(newx,use='pairwise.complete')
+      
+      if (length(nacols) > 1){
+        mu1 <- colMeans(sub1,na.rm=T)
+        mu2 <- colMeans(sub2,na.rm=T)
+      }
+  
+      else {
+        mu1 <- mean(sub1,na.rm=T)
+        mu2 <-colMeans(sub2,na.rm=T)
+      }
+      
+      sigma12 <- covy[(length(nacols)+1):ncol(x),1:length(nacols)]
+      sigma22 <- covy[(length(nacols)+1):ncol(x),(length(nacols)+1):ncol(x)]
+      
+      munew <- mu1 + sigma12%*%solve(sigma22)%*%(sub2[i,]-mu2)
+      #nval<-c()
+      #xrest<-c()
+      #for (j in 1:length(nacols)) {
+      #  temp <- length(na.omit(sub1[,j]))
+      #  temp2<-sum(na.omit(sub1[,j]))
+      #  nval <- append(nval,temp)
+      #  xrest<-append(xrest,temp2)
+      #}
+      #newdata<- munew*(nval+1)-xrest
+      #
+      x[i,nacols] <- munew
+    }
+
   }
   
-  
+  return(x)
   
   }
